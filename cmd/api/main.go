13ed49c01dbedde0b5f4158f887dd61bdb82a617
IsmaCHup <annym.gg@mail.ru>
2025-12-21 00:33:15 +0300

    перенес логгирование в папку config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/IslamCHup/coworking-manager-project/internal/config"
-	"github.com/IslamCHup/coworking-manager-project/internal/logging"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	logger := logging.InitLogger()
+	logger := config.InitLogger()
 
 	db := config.SetupDataBase(logger)
+	if db == nil {
+		logger.Error("database setup failed: db is nil")
+	}
 
 	if err := db.AutoMigrate(); err != nil {
+		logger.Error("auto migrate failed", "error", err)
 	}
 
 	r := gin.Default()
 
+	logger.Info("starting HTTP server", "port", os.Getenv("PORT"))
 	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
-		log.Fatalf("не удалось запустить HTTP-сервер: %v", err)
+		logger.Error("не удалось запустить HTTP-сервер", "err", err)
 	}
 }
