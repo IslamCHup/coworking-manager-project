65e2b78f8aee6417f254365a2869632f73b25444
IsmaCHup <annym.gg@mail.ru>
2025-12-20 14:20:17 +0300

    start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/IslamCHup/coworking-manager-project
+
+go 1.25.1
---
+++ internal/config/database.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDataBase() {
+
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", dbHost, dbUser, dbPass, dbName, dbPort)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{})
+}
