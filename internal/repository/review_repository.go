610cf21c80a06775ece5109f340b3f54152f1254
amir-hayd <amirhaidaev@icloud.com>
2025-12-21 17:59:26 +0300

    сделал интерфейс для репо ,сущностней booking,review,также исправил ошибки синтаксиса после код ревью

--- internal/repository/review_repository.go
+++ internal/repository/review_repository.go
@@ -4,7 +4,7 @@ import "github.com/IslamCHup/coworking-manager-project/internal/models"
 
 type ReviewRepository interface {
 	CreateReview(req *models.Review) error
-	UpdateReview(req *models.Review)
+	UpdateReview(req *models.Review) error
 	DeleteReview(id uint) error
 	GetReview(id uint) (*models.Review, error)
 }
